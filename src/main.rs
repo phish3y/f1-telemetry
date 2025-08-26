6e6372963278922b96d8f47de9763a410fce5a23
E <logsdonevan1@gmail.com>
2025-08-26 14:55:51 -0700

    cleanup and remaining match arms

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,6 @@
 use std::{net::UdpSocket, time::Duration};
 
-use bytemuck::{Pod, Zeroable};
-
-#[repr(C, packed)]
-#[derive(Clone, Copy, Debug, Zeroable, Pod)]
-pub struct PacketHeader {
-    pub m_packet_format: u16,
-    pub m_game_year: u8,
-    pub m_game_major_version: u8,
-    pub m_game_minor_version: u8,
-    pub m_packet_version: u8,
-    pub m_packet_id: u8,
-    pub m_session_uid: u64,
-    pub m_session_time: f32,
-    pub m_frame_identifier: u32,
-    pub m_overall_frame_identifier: u32,
-    pub m_player_car_index: u8,
-    pub m_secondary_player_car_index: u8,
-}
-
-#[repr(u8)]
-#[derive(Debug, Clone, Copy)]
-pub enum PacketId {
-    Motion = 0,
-    Session = 1,
-    LapData = 2,
-    Event = 3,
-    Participants = 4,
-    CarSetups = 5,
-    CarTelemetry = 6,
-    CarStatus = 7,
-    FinalClassification = 8,
-    LobbyInfo = 9,
-    CarDamage = 10,
-    SessionHistory = 11,
-    TyreSets = 12,
-    MotionEx = 13,
-    TimeTrial = 14,
-    LapHistory = 15,
-    LapPositions = 16,
-}
-
-#[repr(C, packed)]
-#[derive(Clone, Copy, Debug, Zeroable, Pod)]
-pub struct CarMotionData {
-    pub m_world_position_x: f32,
-    pub m_world_position_y: f32,
-    pub m_world_position_z: f32,
-
-    pub m_world_velocity_x: f32,
-    pub m_world_velocity_y: f32,
-    pub m_world_velocity_z: f32,
-
-    pub m_world_forward_dir_x: i16,
-    pub m_world_forward_dir_y: i16,
-    pub m_world_forward_dir_z: i16,
-
-    pub m_world_right_dir_x: i16,
-    pub m_world_right_dir_y: i16,
-    pub m_world_right_dir_z: i16,
-
-    pub m_g_force_lateral: f32,
-    pub m_g_force_longitudinal: f32,
-    pub m_g_force_vertical: f32,
-
-    pub m_yaw: f32,
-    pub m_pitch: f32,
-    pub m_roll: f32,
-}
-
-#[repr(C, packed)]
-#[derive(Clone, Copy, Debug, Zeroable, Pod)]
-pub struct PacketMotionData {
-    pub m_header: PacketHeader,
-    pub m_car_motion_data: [CarMotionData; 22],
-}
+mod packet;
 
 fn main() {
     env_logger::init();
@@ -90,24 +16,127 @@ fn main() {
         let (amt, _src) = socket.recv_from(&mut buf).unwrap();
 
         log::info!("recv {} bytes", amt);
-        let header: &PacketHeader = bytemuck::from_bytes(&buf[..std::mem::size_of::<PacketHeader>()]);
+        let header: &packet::header::PacketHeader = bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::header::PacketHeader>()]);
 
-        match header.m_packet_id {
-            0 => {
-                if amt >= std::mem::size_of::<PacketMotionData>() {
-                    let motion: &PacketMotionData =
-                        bytemuck::from_bytes(&buf[..std::mem::size_of::<PacketMotionData>()]);
-                    let player_index = motion.m_header.m_player_car_index as usize;
-                    let player_car_motion = &motion.m_car_motion_data[player_index];
-
-                    let pos_x = player_car_motion.m_world_position_x;
-                    let pos_y = player_car_motion.m_world_position_y;
-                    let pos_z = player_car_motion.m_world_position_z;
-
-                    log::info!("({},{},{})", pos_x, pos_y, pos_z)
+        match packet::header::PacketId::try_from(header.m_packet_id) {
+            Ok(packet::header::PacketId::CarDamage) => {
+                if amt >= std::mem::size_of::<packet::payload::car_damage::PacketCarDamage>() {
+                    let _damage: &packet::payload::car_damage::PacketCarDamage =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::car_damage::PacketCarDamage>()]);
+                    log::info!("Car damage packet received");
                 }
             }
-            _ => {}
+            Ok(packet::header::PacketId::CarSetups) => {
+                if amt >= std::mem::size_of::<packet::payload::car_setups::PacketCarSetups>() {
+                    let _car_setups: &packet::payload::car_setups::PacketCarSetups =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::car_setups::PacketCarSetups>()]);
+                    log::info!("Car setups packet received");
+                }
+            }
+            Ok(packet::header::PacketId::CarStatus) => {
+                if amt >= std::mem::size_of::<packet::payload::car_status::PacketCarStatus>() {
+                    let _car_status: &packet::payload::car_status::PacketCarStatus =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::car_status::PacketCarStatus>()]);
+                    log::info!("Car status packet received");
+                }
+            }
+            Ok(packet::header::PacketId::CarTelemetry) => {
+                if amt >= std::mem::size_of::<packet::payload::car_telemetry::PacketCarTelemetry>() {
+                    let _telemetry: &packet::payload::car_telemetry::PacketCarTelemetry =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::car_telemetry::PacketCarTelemetry>()]);
+                    log::info!("Car telemetry packet received");
+                }
+            }
+            Ok(packet::header::PacketId::Event) => {
+                if amt >= std::mem::size_of::<packet::payload::event::PacketEvent>() {
+                    let _event: &packet::payload::event::PacketEvent =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::event::PacketEvent>()]);
+                    log::info!("Event packet received");
+                }
+            }
+            Ok(packet::header::PacketId::FinalClassification) => {
+                if amt >= std::mem::size_of::<packet::payload::final_classification::PacketFinalClassification>() {
+                    let _final_classification: &packet::payload::final_classification::PacketFinalClassification =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::final_classification::PacketFinalClassification>()]);
+                    log::info!("Final classification packet received");
+                }
+            }
+            Ok(packet::header::PacketId::LapData) => {
+                if amt >= std::mem::size_of::<packet::payload::lap::PacketLap>() {
+                    let _lap: &packet::payload::lap::PacketLap =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::lap::PacketLap>()]);
+                    log::info!("Lap data packet received");
+                }
+            }
+            Ok(packet::header::PacketId::LapHistory) => {
+                log::info!("Lap history packet received (placeholder - no struct defined)");
+            }
+            Ok(packet::header::PacketId::LapPositions) => {
+                if amt >= std::mem::size_of::<packet::payload::lap_positions::PacketLapPositions>() {
+                    let _lap_positions: &packet::payload::lap_positions::PacketLapPositions =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::lap_positions::PacketLapPositions>()]);
+                    log::info!("Lap positions packet received");
+                }
+            }
+            Ok(packet::header::PacketId::LobbyInfo) => {
+                if amt >= std::mem::size_of::<packet::payload::lobby_info::PacketLobbyInfo>() {
+                    let _lobby_info: &packet::payload::lobby_info::PacketLobbyInfo =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::lobby_info::PacketLobbyInfo>()]);
+                    log::info!("Lobby info packet received");
+                }
+            }
+            Ok(packet::header::PacketId::Motion) => {
+                if amt >= std::mem::size_of::<packet::payload::motion::PacketMotion>() {
+                    let motion: &packet::payload::motion::PacketMotion =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::motion::PacketMotion>()]);
+                    log::info!("Motion packet received");
+                }
+            }
+            Ok(packet::header::PacketId::MotionEx) => {
+                if amt >= std::mem::size_of::<packet::payload::motion_ex::PacketMotionEx>() {
+                    let _motion_ex: &packet::payload::motion_ex::PacketMotionEx =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::motion_ex::PacketMotionEx>()]);
+                    log::info!("Motion Ex packet received");
+                }
+            }
+            Ok(packet::header::PacketId::Participants) => {
+                if amt >= std::mem::size_of::<packet::payload::participants::PacketParticipants>() {
+                    let _participants: &packet::payload::participants::PacketParticipants =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::participants::PacketParticipants>()]);
+                    log::info!("Participants packet received");
+                }
+            }
+            Ok(packet::header::PacketId::Session) => {
+                if amt >= std::mem::size_of::<packet::payload::session::PacketSession>() {
+                    let _session: &packet::payload::session::PacketSession =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::session::PacketSession>()]);
+                    log::info!("Session packet received");
+                }
+            }
+            Ok(packet::header::PacketId::SessionHistory) => {
+                if amt >= std::mem::size_of::<packet::payload::session_history::PacketSessionHistory>() {
+                    let _session_history: &packet::payload::session_history::PacketSessionHistory =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::session_history::PacketSessionHistory>()]);
+                    log::info!("Session history packet received");
+                }
+            }
+            Ok(packet::header::PacketId::TimeTrial) => {
+                if amt >= std::mem::size_of::<packet::payload::time_trial::PacketTimeTrial>() {
+                    let _time_trial: &packet::payload::time_trial::PacketTimeTrial =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::time_trial::PacketTimeTrial>()]);
+                    log::info!("Time trial packet received");
+                }
+            }
+            Ok(packet::header::PacketId::TyreSets) => {
+                if amt >= std::mem::size_of::<packet::payload::tyre_sets::PacketTyreSets>() {
+                    let _tyre_sets: &packet::payload::tyre_sets::PacketTyreSets =
+                        bytemuck::from_bytes(&buf[..std::mem::size_of::<packet::payload::tyre_sets::PacketTyreSets>()]);
+                    log::info!("Tyre sets packet received");
+                }
+            }
+            Err(_) => {
+                log::error!("unknown packet id: {}", header.m_packet_id);
+            }
         }
     }
 }
